36aece51a9a29ab60ea94cc119e33123869e9ca8
Heisenberg <omaradelawad20013@gmail.com>
2023-09-04 02:18:20 +0300

    added react toastify main settings.. color should be changed later tho

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,38 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import ErrorPage from './pages/ErrorPage';
+
+import Register from './pages/Register';
+import LogIn from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+
 const App: React.FC = () => {
   return (
-    <div></div>
-  )
-}
-
-export default App
+    <>
+      <Routes>
+        <Route path='*' element={<ErrorPage />} />
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<LogIn />}/>
+        <Route path='/forgot-password' element={<ForgotPassword />}/>
+      </Routes>
+      <ToastContainer
+        position='bottom-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+        progressStyle={{ backgroundColor: '#D9083A' }}
+      />
+    </>
+  );
+};
+  
+  export default App;
