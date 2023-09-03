b7ce9019924a2e25bf470c4dc185e1bef2501d0a
Heisenberg <omaradelawad20013@gmail.com>
2023-09-04 02:00:23 +0300

    installed tailwind, react router and react toastify

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primaryGreen: '#28a745',
+        darkGreen: '#11862c',
+      }
+    }
+  },
+  plugins: [],
+}
