7c32e15eb08000e6c6583e94af3c395845892cd8
Heisenberg <omaradelawad20013@gmail.com>
2023-09-04 13:52:00 +0300

    created Button and Input components

---
+++ frontend/src/ui/Input.tsx
@@ -0,0 +1,33 @@
+type Props = {
+  id: string;
+  type: string;
+  value: string;
+  onChange: (text: string) => void;
+}
+
+const Input: React.FC<Props> = ({ id, type, value, onChange }) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <div className="relative">
+      <label
+        className="absolute -top-2 left-3 px-1 bg-white text-gray-500 text-sm font-bold tracking-wide"
+        htmlFor={id}
+      >
+        Email
+      </label>
+      <input
+        className="appearance-none border border-gray-300 rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none"
+        id={id}
+        type={type}
+        value={value}
+        onChange={handleInputChange}
+        autoComplete='off'
+      />
+    </div>
+  );
+};
+
+export default Input;
