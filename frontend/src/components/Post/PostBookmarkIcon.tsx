a9a41c0810d4c6ad750bedee4412bdbe6c3e309a
Heisenberg <omaradelawad20013@gmail.com>
2023-09-06 03:52:30 +0300

    added better style and functionality to the post bookmark icon

--- frontend/src/components/Post/PostBookmarkIcon.tsx
+++ frontend/src/components/Post/PostBookmarkIcon.tsx
@@ -1,8 +1,25 @@
-import {BsBookmark} from 'react-icons/bs';
+import { useState } from 'react';
+import { BsBookmark, BsBookmarkFill } from 'react-icons/bs';
 
 const PostBookmarkIcon = () => {
+  // This should be done in the parent component actually
+  const [inBookmarks, setInBookmarks] = useState(false);
+
+  const toggleBookmark = () => {
+    setInBookmarks(prevState => !prevState);
+  };
+
+  const classes = 'text-xl cursor-pointer transition duration-500 hover:scale-110';
+
   return (
-    <BsBookmark className='text-xl cursor-pointer transition duration-500 hover:text-indigo-700' />
+    <>
+      {
+        inBookmarks ?
+          <BsBookmarkFill className={classes + ' text-indigo-700  hover:text-indigo-800'} onClick={toggleBookmark} />
+        :
+          <BsBookmark className={classes + ' hover:text-indigo-700'} onClick={toggleBookmark} />
+      }
+    </>
   );
 };
 
