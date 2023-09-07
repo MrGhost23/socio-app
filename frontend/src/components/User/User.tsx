35d10247efee95bb219b1236120afb12e641d1de
Heisenberg <omaradelawad20013@gmail.com>
2023-09-07 17:35:33 +0300

    changed variables names

--- frontend/src/components/User/User.tsx
+++ frontend/src/components/User/User.tsx
@@ -11,13 +11,13 @@ type Props = {
 };
 
 const SuggestedUser: React.FC<Props> = ({ image, id, fullName, followers, changeStyle }) => {
-  const x = 'flex items-center gap-2'
-  const z = 'flex flex-col text-gray-600'
+  const mainContainerClasses = 'flex items-center gap-2'
+  const infoContainerClasses = 'flex flex-col text-gray-600'
 
   return (
-    <div className={changeStyle ? x + ' flex-col xl:flex-row' : x + ' flex-row'}>
+    <div className={changeStyle ? mainContainerClasses + ' flex-col xl:flex-row' : mainContainerClasses + ' flex-row'}>
       <UserImage className='w-16 h-16 !mb-0' src={image} alt={fullName} id={id} />
-      <div className={changeStyle ? z + ' items-center xl:items-start' : z}>
+      <div className={changeStyle ? infoContainerClasses + ' items-center xl:items-start' : infoContainerClasses}>
         <UserFullName className='!text-base font-medium whitespace-nowrap' fullName={fullName} id={id} />
         <p className='text-sm whitespace-nowrap'>{followers} followers</p>
         <Button text='Follow' className='!w-fit !h-fit !px-0 !py-0 !bg-transparent !text-sm !text-indigo-700 !rounded-none' />
