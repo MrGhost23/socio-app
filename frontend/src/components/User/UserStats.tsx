ffcab88546de4f900f693e3311aac671ebc71f4f
Heisenberg <omaradelawad20013@gmail.com>
2023-09-08 18:06:28 +0300

    increased the stats number font size in the profile page

--- frontend/src/components/User/UserStats.tsx
+++ frontend/src/components/User/UserStats.tsx
@@ -14,12 +14,12 @@ const UserStats: React.FC<Props> = (props) => {
     <div className='mb-5 flex flex-row gap-3 text-gray-500 font-medium tracking-wide'>
       <Link to={`${path}/followers`} className='flex flex-col gap-1'>
         <p>Followers</p>
-        <p className='text-lg font-semibold'>{props.followers}</p>
+        <p className='text-xl font-semibold'>{props.followers}</p>
       </Link>
       <div className='w-0.5 h-16 bg-gray-200'></div>
       <Link to={`${path}/following`} className='flex flex-col gap-1'>
         <p>Following</p>
-        <p className='text-lg font-semibold'>{props.following}</p>
+        <p className='text-xl font-semibold'>{props.following}</p>
       </Link>
     </div>
   );
