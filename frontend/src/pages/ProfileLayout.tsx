9f7d154ff4f65a6295cd1580d7ff927550165258
Heisenberg <omaradelawad20013@gmail.com>
2023-09-08 17:22:49 +0300

    created the dropdown menu in the profile

--- frontend/src/pages/ProfileLayout.tsx
+++ frontend/src/pages/ProfileLayout.tsx
@@ -10,6 +10,17 @@ const ProfileLayout = () => {
   const navigate = useNavigate();
 
   const [isFollowing, setIsFollowing] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const blockHandler = () => {
+    console.log('Blocked User');
+    setMenuOpened(false);
+  }
+
+  const ReportHandler = () => {
+    console.log('Reported User');
+    setMenuOpened(false);
+  }
 
   const userInfo = {
     id: '142281728172',
@@ -71,12 +82,33 @@ const ProfileLayout = () => {
   return (
     <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 lg:gap-12 mx-4 sm:mx-10 md:mx-20 my-10">
       <div className="col-span-2 lg:col-span-1 order-1">
-        <Card className="sticky top-32 p-8 flex flex-col items-center">
-          <BsThreeDotsVertical className="absolute top-10 right-6 text-xl text-gray-500 cursor-pointer transition duration-500 hover:text-sky-500" />
+        <Card className="sticky top-32 px-10 py-8 flex flex-col items-center">
+          <div className="relative top-0 right-2 left-full self-start">
+            <BsThreeDotsVertical
+                className={
+                  menuOpened ?
+                    "absolute text-xl text-sky-500 cursor-pointer"
+                  :
+                    "absolute text-xl text-gray-500 cursor-pointer transition duration-500 hover:text-sky-500"
+                }
+                onClick={() => setMenuOpened(prevState => !prevState)}  
+              />
+              {
+                menuOpened &&
+                <ul className="absolute top-7 -right-2 md:translate-x-full px-6 py-4 bg-white rounded border border-gray-10 shadow-md flex flex-col gap-4">
+                  <li>
+                    <Button text='Block' bg={false} onClick={blockHandler} />
+                  </li>
+                  <li>
+                    <Button text='Report' bg={false} onClick={ReportHandler} />
+                  </li>
+                </ul>
+              }
+          </div>
           <UserInfo userInfo={userInfo} />
           <div className="w-full flex flex-col gap-4">
-            <Button text={isFollowing ? "Unfollow" : "Follow"} onClick={() => setIsFollowing(prevState => !prevState)} />
-            <Button text="Edit profile" onClick={() => navigate("/settings")} />
+            <Button text={isFollowing ? "Unfollow" : "Follow"} onClick={() => setIsFollowing(prevState => !prevState)} bg={true}  />
+            <Button text="Edit profile" onClick={() => navigate("/settings")} bg={true}  />
           </div>
         </Card>
       </div>
@@ -85,7 +117,7 @@ const ProfileLayout = () => {
           <Outlet />
         </div>
         <div className="mb-8 xl:mb-0 xl:col-span-1 order-1 xl:order-2">
-          <Card className="sticky top-32 px-8 py-4 pb-6 flex flex-col !text-left">
+          <Card className="sticky top-32 -z-10 px-8 py-4 pb-6 flex flex-col !text-left">
             <h3 className="mb-5 text-xl">Recent Activities</h3>
             <RecentActivities userFirstName={userInfo.firstName} recentActivities={recentActivities} />
           </Card>
