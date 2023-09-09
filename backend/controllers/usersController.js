0bc752ca0accf9018263542c93d65ad6d9011e6b
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-09 12:54:57 +0300

    fetch by username

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -3,7 +3,8 @@ const User = require("../models/User.js");
 const getUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const user = await User.findById(id);
+    const user = await User.find({ username: id }).select("-password");
+    console.log(user);
     res.status(200).json(user);
   } catch (error) {
     res.status(404).json({ message: error.message });
