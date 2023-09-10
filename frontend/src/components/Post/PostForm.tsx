db151b6647f8700a7ef8b6c6231c282d66f19889
Heisenberg <omaradelawad20013@gmail.com>
2023-09-10 16:04:25 +0300

    added image upload input i guess..

--- frontend/src/components/Post/PostForm.tsx
+++ frontend/src/components/Post/PostForm.tsx
@@ -1,12 +1,25 @@
 import { BsCameraVideo, BsImage } from "react-icons/bs";
 import Card from "../../ui/Card";
-import React from "react";
+import React, { useState } from "react";
+import { MdOutlineClose } from "react-icons/md";
 
 type Props = {
   src: string | undefined;
 };
 
 const PostForm: React.FC<Props> = ({ src }) => {
+  const [media, setMedia] = useState<object>();
+
+  const uploadMediaHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+    console.log(file)
+    setMedia(file);
+  }
+
+  const removeMediaHandler = () => {
+    setMedia({});
+  }
+
   return (
     <Card>
       <div className="mb-8 mx-10 py-6 flex flex-col items-center lg:items-start lg:flex-row gap-4">
@@ -17,15 +30,27 @@ const PostForm: React.FC<Props> = ({ src }) => {
             placeholder="Share something.."
           />
           <div className="flex flex-row gap-3">
-            <div className="flex flex-row items-center gap-1 border rounded-xl px-5 py-2 cursor-pointer transition duration-500 hover:text-white hover:bg-sky-500">
+            <div className="relative flex flex-row items-center gap-1 border rounded-xl px-5 py-2 cursor-pointer transition duration-500 hover:text-white hover:bg-sky-500">
               <BsImage />
               <span className="text-sm font-semibold tracking-wide">Photo</span>
+              <input className="absolute top-0 right-0 w-full h-full opacity-0" type="file" value="" onChange={uploadMediaHandler} />
             </div>
-            <div className="flex flex-row items-center gap-1 border rounded-xl px-5 py-2 cursor-pointer transition duration-500 hover:text-white hover:bg-sky-500">
+            <div className="relative flex flex-row items-center gap-1 border rounded-xl px-5 py-2 cursor-pointer transition duration-500 hover:text-white hover:bg-sky-500">
               <BsCameraVideo />
               <span className="text-sm font-semibold tracking-wide">Video</span>
+              <input className="absolute top-0 right-0 w-full h-full opacity-0" type="file" />
             </div>
           </div>
+            {
+              media?.size > 0 && URL.createObjectURL(media) &&
+              <div className="mt-3 relative rounded-lg overflow-hidden">
+                <div className="absolute top-0 left-0 w-full h-full flex flex-row items-center justify-center grow opacity-0 transition duration-500 hover:opacity-100">
+                  <MdOutlineClose className="text-3xl text-white z-50" onClick={removeMediaHandler} />
+                  <div className="absolute w-full h-full bg-black bg-opacity-20"></div>
+                </div>
+                <img className="w-16 h-16" src={URL.createObjectURL(media)} alt="" />
+              </div>
+            }
         </div>
       </div>
     </Card>
