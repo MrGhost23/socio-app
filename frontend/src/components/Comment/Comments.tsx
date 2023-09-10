ed28d9ae8579b2f0a7ddd89784b3aee9cb7c3498
Heisenberg <omaradelawad20013@gmail.com>
2023-09-10 16:30:15 +0300

    set the limit of visible comments to 2 and added a show all functionality

--- frontend/src/components/Comment/Comments.tsx
+++ frontend/src/components/Comment/Comments.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { FaChevronUp } from "react-icons/fa";
 import Comment from "./Comment";
 
 type Props = {
@@ -12,14 +14,37 @@ type Props = {
 }
 
 const Comments: React.FC<Props> = ({ comments }) => {
+  
+  const max = 2;
+  const [commentsSliced, setCommentsSliced] = useState(comments.length > max)
+  
+  const showAll = () => {
+    setCommentsSliced(false);
+  }
+  
   if (!comments) return;
 
   return (
     <div className="mb-5 flex flex-col gap-4">
       {
-        comments.map(comment =>
-          <Comment key={comment.id} comment={comment} />  
-        )
+        commentsSliced ?
+         (
+           <>
+            <p className='flex flex-row items-center gap-2 text-base text-gray-500 font-semibold cursor-pointer transition duration-500 hover:text-sky-500' onClick={showAll}>
+              <FaChevronUp />
+              Show all
+            </p>
+            {
+              comments.slice(-2).map(comment =>
+                <Comment key={comment.id} comment={comment} />  
+              )
+            }
+          </>
+         )
+        :
+          comments.map(comment =>
+            <Comment key={comment.id} comment={comment} />  
+          )
       }
     </div>
   );
