ba4864f28bff0db4bccfd5e5d9335b687d426f4e
Heisenberg <omaradelawad20013@gmail.com>
2023-09-11 13:25:51 +0300

    changed the grid layout if the mode is not follow

--- frontend/src/components/User/Users.tsx
+++ frontend/src/components/User/Users.tsx
@@ -11,8 +11,10 @@ type Props = {
 };
 
 const Users:React.FC<Props> = ({ users, mode }) => {
+  const classes = mode === "follow" ? "md:grid-cols-2" : "lg:grid-cols-2 xl:grid-cols-1";
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+    <div className={`grid grid-cols-1 ${classes} gap-8`}>
       {
         users.map(user =>
           <User key={user.id} id={user.id} image={user.image} fullName={user.fullName} followers={user.followers} changeStyle={false} mode={mode} />
