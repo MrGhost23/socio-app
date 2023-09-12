8b2b3c018d881e036d2f99f0064064a33f526d65
Heisenberg <omaradelawad20013@gmail.com>
2023-09-12 15:39:29 +0300

    updated comment responsiveness

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -16,8 +16,8 @@ const Comment: React.FC<Props> = ({ comment }) => {
   return (
     <div className="flex flex-row items-start gap-2">
       <UserImage className="min-w-[2.5rem] min-h-[2.5rem] w-10 h-10 !mb-0" src={comment.authorImage} alt={comment.authorFullName} id={comment.authorId} />
-      <div className="flex flex-col gap-0.5">
-        <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-col gap-2 sm:gap-0.5">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:gap-2">
           <UserFullName className="!text-base" fullName={comment.authorFullName} id={comment.authorId} />
           <span className="text-sm text-gray-600 tracking-tight">{comment.date}</span>
         </div>
