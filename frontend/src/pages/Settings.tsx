63c6ea8f597f9d590c65b6252e9dd626dcb650a2
Heisenberg <omaradelawad20013@gmail.com>
2023-09-12 19:14:47 +0300

    update settings design

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -90,17 +90,28 @@ const Settings = () => {
       <div className="col-span-3 py-10 flex flex-col xl:grid xl:grid-cols-3 gap-8 xl:gap-16">
         <Card className="!text-left p-8 col-span-2">
           <h3 className="mb-5 text-xl">Account Info</h3>
-          <div className="relative w-fit mb-5 rounded-full overflow-hidden">
-            <img
-              className="w-24 h-24"
-              src={previewImage || currentUser?.userPicture || noAvatar}
-              alt=""
-            />
-            <input
-              className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer"
-              type="file"
-              value=""
-              onChange={uploadImageHandler}
+          <div className="flex flex-row justify-center">
+            <div className="relative w-fit mb-5 rounded-full overflow-hidden">
+              <img
+                className="w-24 h-24"
+                src={previewImage || currentUser?.userPicture || noAvatar}
+                alt=""
+              />
+              <input
+                className="absolute top-0 right-0 w-full h-full opacity-0 cursor-pointer"
+                type="file"
+                value=""
+                onChange={uploadImageHandler}
+              />
+            </div>
+          </div>
+          <div className="mb-5 grid gri-cols-1">
+            <Textarea
+              label="Bio"
+              id="bio"
+              value={bio}
+              onChange={(prev) => setBio(prev)}
+              placeholder="Non sunt magna esse ea velit sint laborum irure sint minim ut excepteur mollit nulla."
             />
           </div>
           <div className="mb-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
@@ -144,17 +155,6 @@ const Settings = () => {
               type="text"
               placeholder="Web Dev."
             />
-          </div>
-          <div className="mb-5 grid gri-cols-1">
-            <Textarea
-              label="Bio"
-              id="bio"
-              value={bio}
-              onChange={(prev) => setBio(prev)}
-              placeholder="Non sunt magna esse ea velit sint laborum irure sint minim ut excepteur mollit nulla."
-            />
-          </div>
-          <div className="mb-5 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
             <Input
               label="Password"
               id="password"
@@ -163,6 +163,8 @@ const Settings = () => {
               type="password"
               placeholder="***************"
             />
+          </div>
+          <div className="mb-5">
             <Button text="Save" bg={true} onClick={submitHandler} />
           </div>
         </Card>
