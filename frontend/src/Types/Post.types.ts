e63a455279d5e6e80cbf33c056016b882b1ba630
Heisenberg <omaradelawad20013@gmail.com>
2023-09-12 19:07:27 +0300

    nothing.. just removed spaces

--- frontend/src/Types/Post.types.ts
+++ frontend/src/Types/Post.types.ts
@@ -1,14 +1,14 @@
 export interface PostType {
-    _id: string | any;
-    userId: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    description: string;
-    userPicture: string;
-    postImage: string;
-    likes: Record<string, any> | any;
-    comments: any[];
-    createdAt: string | Date;
-    updatedAt: string;
-  }
+  _id: string | any;
+  userId: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  description: string;
+  userPicture: string;
+  postImage: string;
+  likes: Record<string, any> | any;
+  comments: any[];
+  createdAt: string | Date;
+  updatedAt: string;
+}
