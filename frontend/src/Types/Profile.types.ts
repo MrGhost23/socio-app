a69940c00a061297854c55fd0e4535d15ab88472
Heisenberg <omaradelawad20013@gmail.com>
2023-09-12 19:09:14 +0300

    added bookmarks & blocked to the profile interface

--- frontend/src/Types/Profile.types.ts
+++ frontend/src/Types/Profile.types.ts
@@ -7,8 +7,10 @@ export interface ProfileType {
   country: string;
   userId: string;
   createdAt: Date | string;
+  bookmarks: string[];
   following: string[];
   followers: string[];
+  blocked: string[];
   userPicture?: string;
   occupation?: string;
   bio?: string;
