b7f6009ab64b1c730e0d67b6488b8b6fe3884da6
Heisenberg <omaradelawad20013@gmail.com>
2023-09-12 09:23:12 +0300

    removed a console log

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -27,7 +27,6 @@ const Post: React.FC<Props> = ({
   currentUserImage,
   post,
 }) => {
-  console.log(Object.keys(post.likes).length)
   return (
     <Card className="px-8 py-6 !text-left">
       <div className="relative mb-2 flex flex-row justify-between gap-3">
