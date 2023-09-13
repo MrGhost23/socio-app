a8c272ca548f21f0758d6e70d5a2da073215dae9
Heisenberg <omaradelawad20013@gmail.com>
2023-09-13 17:06:39 +0300

    finished both followers and following pages

--- frontend/src/pages/Following.tsx
+++ frontend/src/pages/Following.tsx
@@ -1,56 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { UserType } from '../Types/User.types';
 import Users from "../components/User/Users";
 import Card from '../ui/Card';
 import SearchInput from "../ui/SearchInput";
+import Loading from '../ui/Loading';
 
 const Following = () => {
-  const suggestedUsers = [
-    {
-      username: 'MrGhost',
-      image:
-        "https://cdn.discordapp.com/attachments/700993218850062381/1140480477833203742/ce7ca87cc7bd870fc40642fd245b011b.png",
-      firstName: "Omar",
-      lastName: "Mohamed",
-      followers: [],
-    },
-    {
-      username: 'TomasaRunolfsson',
-      image:
-        "https://cdn.discordapp.com/attachments/700993218850062381/1140480078644531220/352a1b49195bfa773765b4fdfb17da42.png",
-      firstName: "Tomasa",
-      lastName: "Runolfsson",
-      followers: [],
-    },
-    {
-      username: 'HubertWhite',
-      image:
-        "https://cdn.discordapp.com/attachments/700993218850062381/1140479590012309534/50e2e84b6427e2112ea02507b5bc849f.png",
-      firstName: "Hubert",
-      lastName: "White",
-      followers: [],
-    },
-    {
-      username: 'AdelbertSawayn',
-      image:
-        "https://cdn.discordapp.com/attachments/700993218850062381/1140477104467742791/6183b49eced8a25862b25a0f2f110f94.png",
-      firstName: "Adelbert",
-      lastName: "Sawayn",
-      followers: [],
-    },
-    {
-      username: 'YvetteMayer',
-      image:
-        "https://cdn.discordapp.com/attachments/700993218850062381/1140476544339427468/38f761c6e7dd7701cacaa81409ffbaa2.png",
-      firstName: "Yvette",
-      lastName: "Mayer",
-      followers: [],
-    },
-  ];
+  const { username } = useParams();
+
+  const [following, setFollowers] = useState<UserType[]>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchPostData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/v1/users/${username}/following`);
+        setFollowers(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+        setError(!!error)
+      }
+      setIsLoading(false);
+    }
+
+    fetchPostData();
+  }, [username])
+
+
+  if (isLoading) return <Loading />;
+  if (error) return <p>An error occurred</p>;
 
   return (
     <Card className="sticky top-32 px-8 py-4 pb-6 flex flex-col !text-left">
       <h3 className="mb-5 text-xl">Following</h3>
       <SearchInput className="mb-5" />
-      <Users users={suggestedUsers} mode='follow' />
+      <Users users={following!} mode='follow' />
     </Card>
   );
 };
