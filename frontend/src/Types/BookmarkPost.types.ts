f28e32a14ac077682f53d7badb3f6ca70dc53626
Heisenberg <omaradelawad20013@gmail.com>
2023-09-13 16:16:58 +0300

    created a type for the bookmark post

---
+++ frontend/src/Types/BookmarkPost.types.ts
@@ -0,0 +1,9 @@
+export interface BookmarkPostType {
+  _id: string;
+  description: string;
+  postImage?: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  userPicture: string;
+}
