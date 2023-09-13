861c4329c899b501776fa3d708e7e0a521042d9a
Heisenberg <omaradelawad20013@gmail.com>
2023-09-13 06:00:58 +0300

    used the backend api in the post page

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,45 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import Post from '../components/Post/Post';
+import { PostType } from "../Types/Post.types";
+import { useParams } from "react-router-dom";
+import Loading from "../ui/Loading";
 
 const PostPage = () => {
-  const currentUserId = '618237294201';
-  const currentUserFullName = "Omar Adel";
-  const currentUserImage =
-    "https://cdn.discordapp.com/avatars/683014296342364286/30889b16f6a06a146378d9d10554582b.png?size=1024";
+  const { id } = useParams();
+  
+  const [postData, setPostData] = useState<PostType>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  const post = {
-    id: '1',
-    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt qui placeat ducimus minima praesentium necessitatibus deserunt repudiandae ut incidunt! Deleniti numquam maxime ipsa, explicabo iure nesciunt dolores perspiciatis deserunt fugiat sapiente placeat repellendus rem, obcaecati dignissimos illum culpa hic sed accusamus fuga, similique quia optio voluptatem nam? Dolor, nostrum omnis!",
-    image:
-      "https://i.pcmag.com/imagery/articles/00Iy7r4lqzdcLM9vUdFIZoN-25..v1628272383.jpg",
-    likes: 250,
-    comments: 40,
-    authorFullName: "Omar Adel",
-    authorTag: "Heisenberg",
-    authorImage:
-      "https://cdn.discordapp.com/avatars/683014296342364286/30889b16f6a06a146378d9d10554582b.png?size=1024",
-    date: "2 hours ago",
-    postComments: [
-      {
-        id: '1',
-        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat vero veritatis eligendi quidem obcaecati quae a nostrum labore dolore nisi!',
-        date: '3 hours ago',
-        authorId: '32893273821',
-        authorFullName: 'Domenica Dicki',
-        authorImage: 'https://cdn.discordapp.com/attachments/700993218850062381/1140479590012309534/50e2e84b6427e2112ea02507b5bc849f.png'
-      },
-      {
-        id: '2',
-        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-        date: '2 hours ago',
-        authorId: '43849384921',
-        authorFullName: 'Chaya Reichert',
-        authorImage: 'https://cdn.discordapp.com/attachments/700993218850062381/1140477104467742791/6183b49eced8a25862b25a0f2f110f94.png'
+  useEffect(() => {
+    const fetchPostData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/v1/posts/${id}`);
+        setPostData(response.data);
+      } catch (error) {
+        console.log(error)
+        setError(!!error)
       }
-    ]
-  };
+      setIsLoading(false);
+    }
+
+    fetchPostData();
+  }, [id])
+  
+  if (isLoading) return <Loading />;
+  if (error) return <p>An error occurred</p>;
 
   return (
-    <Post currentUserId={currentUserId} currentUserFullName={currentUserFullName} currentUserImage={currentUserImage} post={post} />
+    <Post post={postData!} />
   );
 };
 
