ff1e8ac4411edc0fc5b9c7662c61af19ad37b88c
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-14 09:04:31 +0300

    send arr of obj

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -98,7 +98,7 @@ const getFollowers = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json(user[0]);
+    res.status(200).json(user);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
