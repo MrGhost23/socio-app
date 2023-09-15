70a49298a84d048473f90e4857409a0fb1d18104
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-15 16:07:35 +0300

    fixed it ye

--- backend/utils/activityUtils.js
+++ backend/utils/activityUtils.js
@@ -1,9 +1,8 @@
-const { find, deleteOne } = require("../models/Activity");
+const Activity = require("../models/Activity");
 
 const cleanupActivities = async (userId) => {
   try {
-    const userActivities = await find({ userId });
-    userActivities.sort((a, b) => b.timestamp - a.timestamp);
+    const userActivities = await Activity.find({ userId });
 
     if (userActivities.length === 0) {
       return;
@@ -14,7 +13,9 @@ const cleanupActivities = async (userId) => {
         0,
         userActivities.length - 5
       );
-      await deleteOne({ _id: { $in: activitiesToDelete.map((a) => a._id) } });
+      await Activity.deleteOne({
+        _id: { $in: activitiesToDelete.map((a) => a._id) },
+      });
     }
   } catch (error) {
     console.error(error);
