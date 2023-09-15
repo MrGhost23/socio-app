8da16ee81fc481f6de5c6539062d4b5f23cebb5a
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-15 08:11:04 +0300

    add activity router

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const { createPost } = require("./controllers/postsController");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/usersRoutes");
 const postRouter = require("./routes/postsRoutes");
+const activityRouter = require("./routes/activityRoutes");
 const commentRouter = require("./routes/commentsRoutes");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
@@ -27,8 +28,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
 const path = require("path");
 const authenticateUser = require("./middleware/authenticateUser");
-
-// EXPRESS STATIC HERE
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -55,6 +54,7 @@ app.post(
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", activityRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/posts", commentRouter);
 
