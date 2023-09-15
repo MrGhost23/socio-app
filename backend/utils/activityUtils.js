b74c2ffd14dddd387592af8af4d4057ef4f6384e
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-15 06:55:08 +0300

    cleanup func to send the lastest 5 posts and delete the rest in db

--- backend/utils/activityUtils.js
+++ backend/utils/activityUtils.js
@@ -1,12 +1,25 @@
 const Activity = require("../models/Activity");
 
-const cleanupActivities = async (limit) => {
-  const activities = await Activity.find().sort({ timestamp: 1 });
-  if (activities.length > limit) {
-    const activitiesToDelete = activities.slice(0, activities.length - limit);
-    for (const activity of activitiesToDelete) {
-      await Activity.findByIdAndRemove(activity._id);
+const cleanupActivities = async (userId) => {
+  try {
+    const userActivities = await Activity.find({ userId }).sort("-timestamp");
+    if (!userActivities) {
+      return;
     }
+
+    if (userActivities.length > 5) {
+      const activitiesToDelete = userActivities.slice(
+        0,
+        userActivities.length - 5
+      );
+
+      for (const activity of activitiesToDelete) {
+        await Activity.deleteOne({ _id: activity._id });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error cleaning up activities");
   }
 };
 
