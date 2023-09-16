925f82fa37955b5e7e50a0c72ecca6fda42d3d10
Heisenberg <omaradelawad20013@gmail.com>
2023-09-16 18:36:37 +0300

    added the menu component to the user menu

--- frontend/src/components/User/UserMenu.tsx
+++ frontend/src/components/User/UserMenu.tsx
@@ -3,7 +3,7 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 import { ImBlocked } from "react-icons/im";
 import { PiWarningBold } from "react-icons/pi";
 import useProfileActions from "../../hooks/useProfileActions";
-import Button from "../../ui/Button";
+import Menu from "../../ui/Menu";
 
 type Props = {
   isMyProfile: boolean;
@@ -25,41 +25,42 @@ const UserMenu: React.FC<Props> = ({ isMyProfile, profileUsername }) => {
 
   const [menuOpened, setMenuOpened] = useState(false);
 
+  const menuList = [
+    {
+      id: 1,
+      text: "Block",
+      action: toggleBlockHandler,
+      icon: ImBlocked,
+      showIf: true,
+    },
+    {
+      id: 2,
+      text: "Report",
+      action: reportHandler,
+      icon: PiWarningBold,
+      iconClasses: "!text-lg",
+      showIf: true,
+    },
+  ];
+
   return (
     <>
       {!isMyProfile && (
-        <BsThreeDotsVertical
-          className={
+        <Menu
+          isOpen={menuOpened}
+          setIsOpen={setMenuOpened}
+          list={menuList}
+          menuIcon={BsThreeDotsVertical}
+          menuClasses="!top-7 !-right-2 lg:translate-x-full"
+          menuIconClasses={
             menuOpened
-              ? "absolute text-xl text-sky-500 cursor-pointer"
-              : "absolute text-xl text-gray-500 cursor-pointer transition duration-500 hover:text-sky-500"
+              ? "absolute !text-xl text-sky-500 cursor-pointer"
+              : "absolute !text-xl hover:text-sky-500"
           }
-          onClick={() => setMenuOpened((prevState) => !prevState)}
         />
-      )}
-      {menuOpened && (
-        <ul className="absolute top-7 -right-2 md:translate-x-full px-6 py-4 bg-white rounded border border-gray-10 shadow-md flex flex-col gap-4">
-          <li>
-            <Button
-              text="Block"
-              bg={false}
-              onClick={toggleBlockHandler}
-              icon={ImBlocked}
-            />
-          </li>
-          <li>
-            <Button
-              text="Report"
-              bg={false}
-              onClick={reportHandler}
-              icon={PiWarningBold}
-              iconClasses="!text-lg"
-            />
-          </li>
-        </ul>
       )}
     </>
   );
 };
-
+  
 export default UserMenu;
