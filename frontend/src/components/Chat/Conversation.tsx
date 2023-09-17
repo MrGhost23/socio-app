7b11f4dc5d0c73dcbcaa2f928896245862f0789e
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-17 16:04:36 +0300

    Conversation dynamic

--- frontend/src/components/Chat/Conversation.tsx
+++ frontend/src/components/Chat/Conversation.tsx
@@ -1,22 +1,38 @@
-const Conversation = () => {
-  return (
-    <div className="flex flex-row py-4 px-2 justify-center items-center border-b-2">
-      <div className="w-1/4">
-        <img
-          src="https://i.pinimg.com/564x/c7/58/a5/c758a5b04e0e7080dc19187e8c62a9c3.jpg"
-          className="object-cover h-12 w-12 rounded-full"
-          alt=""
-        />
-      </div>
-      <div className="w-full">
-        <div className="flex items-center flex-wrap gap-2">
-          <div className="text-lg font-semibold">Omar Mohamed</div>
-          <span className="text-sm text-gray-500">12:23AM</span>
-        </div>
+import { useEffect } from "react";
+import useUserProfile from "../../hooks/useUserProfile";
+import noAvatar from "../../assets/noAvatar.png";
 
-        <span className="text-gray-500 font-medium">بدخله براحة خالص</span>
-      </div>
-    </div>
+const Conversation = ({ chat, currentUserId }) => {
+  const username = chat.members.find((username) => username !== currentUserId);
+  const { profile, loading } = useUserProfile(username, true);
+  console.log(profile?.firstName);
+
+  return (
+    <>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="flex flex-row py-4 px-2 justify-center items-center border-b-2">
+          <div className="w-1/4">
+            <img
+              src={profile?.userPicture || noAvatar}
+              className="object-cover h-12 w-12 rounded-full"
+              alt={profile?.username}
+            />
+          </div>
+          <div className="w-full">
+            <div className="flex items-center flex-wrap gap-2">
+              <div className="text-lg font-semibold">
+                {profile?.firstName + " " + profile?.lastName}
+              </div>
+              <span className="text-sm text-gray-500">12:23AM</span>
+            </div>
+
+            <span className="text-gray-500 font-medium">بدخله براحة خالص</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default Conversation;
