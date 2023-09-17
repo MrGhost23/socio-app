82e72dadd4c2328ab962bdd2fd246548cb400439
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-18 01:14:42 +0300

    user info in the right side - still on work

--- frontend/src/components/Chat/ChatInfo.tsx
+++ frontend/src/components/Chat/ChatInfo.tsx
@@ -3,32 +3,35 @@ import { selectUser } from "../../store/slices/authSlice";
 import Button from "../../ui/Button";
 import noAvatar from "../../assets/noAvatar.png";
 
-const ChatInfo = () => {
+const ChatInfo = ({ chat, currentUser }) => {
+  const username = chat?.members?.find((username) => username !== currentUser);
   const user = useSelector(selectUser);
   return (
-    <div className="w-2/5 border-l-2 h-[calc(100vh-82px)] px-5 pt-5">
-      <div className="flex flex-col items-center">
-        <img
-          src={user?.userPicture || noAvatar}
-          className="object-cover rounded-full h-44 w-44"
-          alt=""
-        />
-        <div className="font-semibold text-xl text-center pt-4">
-          {user?.firstName + " " + user?.lastName}
+    chat && (
+      <div className="w-2/5 border-l-2 h-[calc(100vh-82px)] px-5 pt-5">
+        <div className="flex flex-col items-center">
+          <img
+            src={user?.userPicture || noAvatar}
+            className="object-cover rounded-full h-44 w-44"
+            alt=""
+          />
+          <div className="font-semibold text-xl text-center pt-4">
+            {user?.firstName + " " + user?.lastName}
+          </div>
+          <div className="font-semibold text-sm text-gray-500 text-center">
+            @{user?.username}
+          </div>
+          <div className="font-semibold py-4">Created 22 Sep 2021</div>
+          <div className="">User Bio Goes Here!</div>
+          <Button
+            text="View Profile"
+            bg={true}
+            className="mt-5"
+            onClick={() => console.log("YOYO")}
+          />
         </div>
-        <div className="font-semibold text-sm text-gray-500 text-center">
-          @{user?.username}
-        </div>
-        <div className="font-semibold py-4">Created 22 Sep 2021</div>
-        <div className="">User Bio Goes Here!</div>
-        <Button
-          text="View Profile"
-          bg={true}
-          className="mt-5"
-          onClick={() => console.log("YOYO")}
-        />
       </div>
-    </div>
+    )
   );
 };
 export default ChatInfo;
