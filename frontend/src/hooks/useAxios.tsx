f6e8347d7626c5a9239115a649861cd2b7b19252
Heisenberg <omaradelawad20013@gmail.com>
2023-09-21 05:13:31 +0300

    updated the useAxios hook

--- frontend/src/hooks/useAxios.tsx
+++ frontend/src/hooks/useAxios.tsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 import axios, { Method } from "axios";
 
-const useAxios = (url: string, method: Method, body?: object) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+interface AxiosResponse<T> {
+  data: T;
+}
+
+const useAxios = <T,>(url: string, method: Method, body?: object) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios({
+        const response: AxiosResponse<T> = await axios({
           method,
           url,
           data: body,
