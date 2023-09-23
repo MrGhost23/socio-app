56b9d0c27af666538287605724da018b0f219841
Heisenberg <omaradelawad20013@gmail.com>
2023-09-23 11:22:41 +0300

    renamed the post skeleton component and made few changes

--- frontend/src/skeletons/PostSkeleton.tsx
+++ frontend/src/skeletons/PostSkeleton.tsx
@@ -1,9 +1,13 @@
 import SkeletonWrapper from "./SkeletonWrapper";
 import SkeletonElement from "./SkeletonElement";
 
-const SkeletonPost: React.FC = () => {
+type Props = {
+  className?: string;
+};
+
+const PostSkeleton: React.FC<Props> = ({ className }) => {
   return (
-    <SkeletonWrapper>
+    <SkeletonWrapper className={className}>
       <div className="skeleton-article">
         <div className="flex flex-col gap-2">
           <div className="relative mb-2 flex flex-row justify-between gap-3">
@@ -12,8 +16,8 @@ const SkeletonPost: React.FC = () => {
               className="min-w-[3rem] sm:min-w-[3.5rem] w-12 sm:w-14 min-h-[3rem] sm:min-h-[3.5rem] h-12 sm:h-14"
             />
             <div className="grow flex flex-col items-start gap-1.5">
-              <SkeletonElement type="name" />
-              <SkeletonElement type="date" />
+              <SkeletonElement type="text" className="max-w-[10rem]" />
+              <SkeletonElement type="text" className="max-w-[8rem]" />
             </div>
           </div>
           <SkeletonElement type="text" repeat={3} />
@@ -23,4 +27,4 @@ const SkeletonPost: React.FC = () => {
   );
 };
 
-export default SkeletonPost;
+export default PostSkeleton;
