b99755a271a78e5ca43422bbd5566545bd4dcd3b
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-23 12:25:44 +0300

    made socket.io on the same backend server

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const http = require("http");
 const cors = require("cors");
 const multer = require("multer");
 const helmet = require("helmet");
@@ -21,8 +22,14 @@ const notificationRouter = require("./routes/notificationRoutes");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const CustomError = require("./errors/index");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3000" },
+});
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(helmet({ crossOriginResourcePolicy: false }));
@@ -99,11 +106,68 @@ app.use("/api/v1/validateToken", authenticateUser, async (req, res) => {
   }
   res.status(200).json({ userData });
 });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  socket.on("new-user-add", (newUsername) => {
+    if (!onlineUsers.some((user) => user.username === newUsername)) {
+      onlineUsers.push({ username: newUsername, socketId: socket.id });
+      console.log("New User Connected", onlineUsers);
+    }
+
+    io.emit("get-users", onlineUsers);
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", onlineUsers);
+
+    io.emit("get-users", onlineUsers);
+  });
+
+  socket.on("send-message", (data) => {
+    const { receiverUsername } = data;
+    const user = onlineUsers.find((user) => user.username === receiverUsername);
+
+    console.log("Sending from socket to:", receiverUsername);
+    console.log("Data:", data);
+    if (user) {
+      io.to(user.socketId).emit("receive-message", data);
+      console.log(user.socketId);
+      console.log("RECEIVED");
+    }
+  });
+
+  socket.on(
+    "sendNotification",
+    ({
+      senderUsername,
+      receiverUsername,
+      actionType,
+      postId,
+      userPicture,
+      firstName,
+      lastName,
+    }) => {
+      const receiver = getUsers(receiverUsername);
+      io.to(receiver.socketId).emit("getNotification", {
+        senderUsername,
+        actionType,
+        postId,
+        userPicture,
+        firstName,
+        lastName,
+      });
+    }
+  );
+});
+
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(port, () =>
+    server.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
