ec71bb9a3e681ce6404fcb4366c33976055a570d
Heisenberg <omaradelawad20013@gmail.com>
2023-09-23 16:06:03 +0300

    minor change in the suggested users

--- frontend/src/components/User/SuggestedUsers.tsx
+++ frontend/src/components/User/SuggestedUsers.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SuggestedUsers: React.FC<Props> = ({ users }) => {
   return (
-    <div className="pb-5 xl:pb-0 flex flex-row xl:flex-col gap-5 md:gap-8 xl:gap-3 overflow-x-auto">
+    <div className="pb-1 xl:pb-0 flex flex-row xl:flex-col gap-5 md:gap-8 xl:gap-3 overflow-x-auto">
       {users.map((user) => (
         <User
           key={user.username}
