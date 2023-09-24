540c59be8d287bdc0d96a0db284b86207fc15b41
Heisenberg <omaradelawad20013@gmail.com>
2023-09-24 18:06:57 +0300

    fixed the sidebar z-index

--- frontend/src/components/Backdrop.tsx
+++ frontend/src/components/Backdrop.tsx
@@ -6,7 +6,7 @@ const Backdrop: React.FC<BackdropProps> = ({ onClick }) => {
   return (
     <div
       onClick={onClick}
-      className={`fixed top-0 left-0 z-999 w-full h-screen bg-black bg-opacity-50 transition-all duration-300`}
+      className={`fixed top-0 left-0 z-30 w-full h-screen bg-black bg-opacity-50 transition-all duration-300`}
     />
   );
 };
