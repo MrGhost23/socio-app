f9e4d52aff3551e9eec28b6183566f4443291b01
Heisenberg <omaradelawad20013@gmail.com>
2023-09-24 17:04:30 +0300

    deleted toggleFollowUser function from the useProfileActions hook

--- frontend/src/hooks/useProfileActions.tsx
+++ frontend/src/hooks/useProfileActions.tsx
@@ -6,16 +6,6 @@ import axios from "axios";
 const useProfileActions = () => {
   const currentUser = useSelector(selectUser);
 
-  const toggleFollowUser = async (username: string) => {
-    try {
-      await axios.put(`http://localhost:5000/api/v1/users/${username}/follow`, {
-        username: currentUser!.username,
-      });
-    } catch (error) {
-      toast.info(`Something went wrong!`);
-    }
-  };
-
   const toggleBlockUser = async (username: string) => {
     try {
       await axios.post(
@@ -38,7 +28,6 @@ const useProfileActions = () => {
   };
 
   return {
-    toggleFollowUser,
     toggleBlockUser,
     reportUser,
   };
