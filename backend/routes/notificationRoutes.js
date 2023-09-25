9fdad427cb555868cc9b241f6793599b01da7f67
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-25 16:00:05 +0300

    create Mark as read Notification Route by id

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const {
   getNotificationsWithUserData,
+  markAsReadNotification,
 } = require("../controllers/notificationController");
 const authenticateUser = require("../middleware/authenticateUser");
 
 router.get("/", authenticateUser, getNotificationsWithUserData);
+router.patch("/:notificationId", authenticateUser, markAsReadNotification);
 
 module.exports = router;
