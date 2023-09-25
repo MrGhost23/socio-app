d2b777895d88ebe64ecfdd5ea8548f718ab6206c
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-25 15:59:43 +0300

    sent _id to the front and create Mark as read Notification controller

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -3,10 +3,11 @@ const Notification = require("../models/Notification");
 const getNotificationsWithUserData = async (req, res) => {
   try {
     const notifications = await Notification.find({ receiver: req.user._id })
-      .populate("sender", "firstName lastName userPicture isRead createdAt")
+      .populate("sender", "firstName lastName userPicture isRead createdAt _id")
       .sort({ createdAt: -1 });
 
     const formattedNotifications = notifications.map((notification) => ({
+      _id: notification._id,
       firstName: notification.sender.firstName,
       lastName: notification.sender.lastName,
       userPicture: notification.sender.userPicture,
@@ -24,6 +25,40 @@ const getNotificationsWithUserData = async (req, res) => {
   }
 };
 
+const markAsReadNotification = async (req, res) => {
+  try {
+    const { notificationId } = req.params;
+    const updatedNotification = await Notification.findByIdAndUpdate(
+      notificationId,
+      { isRead: true },
+      { new: true }
+    )
+      .populate("sender", "firstName lastName userPicture isRead createdAt _id")
+      .sort({ createdAt: -1 });
+
+    if (!updatedNotification) {
+      return res.status(404).json({ message: "Notification not found" });
+    }
+
+    const responseNotification = {
+      _id: updatedNotification._id,
+      firstName: updatedNotification.sender.firstName,
+      lastName: updatedNotification.sender.lastName,
+      userPicture: updatedNotification.sender.userPicture,
+      actionType: updatedNotification.actionType,
+      postId: updatedNotification.postId,
+      isRead: updatedNotification.isRead,
+      createdAt: updatedNotification.createdAt,
+    };
+
+    res.json(responseNotification);
+  } catch (error) {
+    console.error("Error marking notification as read:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 module.exports = {
   getNotificationsWithUserData,
+  markAsReadNotification,
 };
