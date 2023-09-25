0cb1c29beb29279fb52c1d866124ec1830d76c8f
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-25 12:05:28 +0300

    fix jwt malformed crash when logging in

--- backend/middleware/authenticateUser.js
+++ backend/middleware/authenticateUser.js
@@ -2,32 +2,41 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const authenticateUser = async (req, res, next) => {
-  let token = req.header("Authorization");
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
-  if (token.startsWith("Bearer ")) {
-    token = token.slice(7, token.length).trimLeft();
-  }
-  const user = jwt.verify(token, process.env.JWT_SECRET);
-
-  if (!user) {
-    return res.status(401).json({ message: "Token is not valid" });
-  }
-
   try {
-    const userData = await User.findById(user.userId).select("-password");
+    let token = req.header("Authorization");
 
-    if (!userData) {
-      return res.status(404).json({ message: "User not found" });
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    req.user = userData;
 
-    next();
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Token is not valid" });
+      }
+
+      const userId = decoded.userId;
+
+      User.findById(userId)
+        .select("-password")
+        .then((userData) => {
+          if (!userData) {
+            return res.status(404).json({ message: "User not found" });
+          }
+
+          req.user = userData;
+          next();
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+          return res.status(500).json({ message: "Internal server error" });
+        });
+    });
   } catch (error) {
-    console.error("Error fetching user data:", error);
+    console.error("Error verifying token:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
