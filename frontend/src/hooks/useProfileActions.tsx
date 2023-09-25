ff6de8384b7d131660506215269a5ace44b1405e
Heisenberg <omaradelawad20013@gmail.com>
2023-09-25 15:41:35 +0300

    removed the toggleBlockUser function from the useProfileActions hook

--- frontend/src/hooks/useProfileActions.tsx
+++ frontend/src/hooks/useProfileActions.tsx
@@ -1,23 +1,6 @@
-import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import { selectUser } from "../store/slices/authSlice";
-import axios from "axios";
 
 const useProfileActions = () => {
-  const currentUser = useSelector(selectUser);
-
-  const toggleBlockUser = async (username: string) => {
-    try {
-      await axios.post(
-        `http://localhost:5000/api/v1/users/${username}/block-unblock`,
-        {
-          username: currentUser!.username,
-        }
-      );
-    } catch (error) {
-      toast.info(`Something went wrong!`);
-    }
-  };
 
   const reportUser = () => {
     try {
