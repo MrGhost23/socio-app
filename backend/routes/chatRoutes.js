430e3229af589ee408cfd457c142f4ddefc5697f
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-26 03:02:44 +0300

    delete unAllowMessage Route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -3,7 +3,6 @@ const {
   userChats,
   findChat,
   markChatAsRead,
-  unAllowMessage,
 } = require("../controllers/chatController");
 
 const router = require("express").Router();
@@ -11,7 +10,6 @@ const router = require("express").Router();
 router.get("/find/:firstUsername/:secondUsername", findChat);
 router.get("/:username", userChats);
 router.patch("/:chatId", markChatAsRead);
-router.patch("/:chatId/unAllowMessage", unAllowMessage);
 router.post("/", createChat);
 
 module.exports = router;
