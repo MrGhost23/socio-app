61858d7f83e2545ab06cd5bc47b03231294f96f7
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-26 03:01:50 +0300

    delete unAllowMessage, don't need it and made it while blocking - unblock

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -93,30 +93,9 @@ const markChatAsRead = async (req, res) => {
   }
 };
 
-const unAllowMessage = async (req, res) => {
-  try {
-    const { chatId } = req.params;
-    const chat = await Chat.findByIdAndUpdate(
-      chatId,
-      { allowMessage: false },
-      { new: true }
-    );
-
-    if (!chat) {
-      return res.status(404).json({ message: "Chat not found" });
-    }
-
-    res.json(chat);
-  } catch (error) {
-    console.error("Error marking allowMessage false:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
 module.exports = {
   createChat,
   userChats,
   findChat,
   markChatAsRead,
-  unAllowMessage,
 };
