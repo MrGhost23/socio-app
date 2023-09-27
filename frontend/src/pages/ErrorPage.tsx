911aabfd7aad9e0d07d141eddbc715eb65705cd5
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 06:38:47 +0300

    added an auto redirection in the error page after 5 seconds

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,4 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(5);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [navigate]);
+
   return (
     <div className="mt-20 flex flex-col items-center justify-center gap-8">
       <p className="text-4xl text-gray-600 dark:text-textLighter font-bold">
@@ -6,6 +26,9 @@ const ErrorPage: React.FC = () => {
       </p>
       <p className="text-2xl text-gray-500 dark:text-textLight font-semibold">
         This page doesn't exist or you don't have access to it.
+      </p>
+      <p className="text-lg text-gray-500 dark:text-textLight">
+        You will be directed to the home page after {countdown} seconds.
       </p>
     </div>
   );
