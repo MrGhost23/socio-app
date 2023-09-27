9c70c721d907017bae2b9313e4f354ebba599e36
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-27 16:11:14 +0300

    dark theme

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 const ErrorPage: React.FC = () => {
   const navigate = useNavigate();
-  const [countdown, setCountdown] = useState(5);
+  const [countdown, setCountdown] = useState(200);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -12,7 +12,7 @@ const ErrorPage: React.FC = () => {
 
     setTimeout(() => {
       navigate("/");
-    }, 5000);
+    }, 100000);
 
     return () => {
       clearInterval(timer);
@@ -20,14 +20,14 @@ const ErrorPage: React.FC = () => {
   }, [navigate]);
 
   return (
-    <div className="mt-20 flex flex-col items-center justify-center gap-8">
+    <div className="mt-20 flex flex-col items-center justify-center gap-8 h-[calc(100vh-82px)]">
       <p className="text-4xl text-gray-600 dark:text-textLighter font-bold">
         An error occurred!
       </p>
-      <p className="text-2xl text-gray-500 dark:text-textLight font-semibold">
+      <p className="text-2xl text-gray-500 dark:text-textLight font-semibold px-5 text-center">
         This page doesn't exist or you don't have access to it.
       </p>
-      <p className="text-lg text-gray-500 dark:text-textLight">
+      <p className="text-lg text-gray-500 dark:text-textLight px-5 text-center">
         You will be directed to the home page after {countdown} seconds.
       </p>
     </div>
