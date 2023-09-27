9aa26d35a49e7d9a9fd74cb86f3ec3c5bf8a6eb6
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 08:29:02 +0300

    wrapped the ChatInfo component inside a memo because there was some rerenders idk why

--- frontend/src/components/Chat/ChatInfo.tsx
+++ frontend/src/components/Chat/ChatInfo.tsx
@@ -1,3 +1,4 @@
+import React, { memo } from "react";
 import { useNavigate } from "react-router-dom";
 import { MdClose } from "react-icons/md";
 import { ProfileType } from "../../Types/Profile.types";
@@ -13,43 +14,42 @@ type Props = {
   hideUserInfo: () => void;
 };
 
-const ChatInfo: React.FC<Props> = ({
-  receiverUsername,
-  receiverData,
-  hideUserInfo,
-}) => {
-  const navigate = useNavigate();
+const ChatInfo: React.FC<Props> = memo(
+  ({ receiverUsername, receiverData, hideUserInfo }) => {
+    const navigate = useNavigate();
 
-  return (
-    <div className="relative flex flex-col items-center pt-8 lg:pt-0">
-      <UserImage
-        className="min-w-[9rem] w-36 min-h-[9rem] h-36 mb-4"
-        src={receiverData?.userPicture}
-        username={receiverUsername}
-        link={true}
-      />
-      <UserFullName
-        className="font-semibold text-xl"
-        fullName={
-          (receiverData?.firstName || "Socio") +
-          " " +
-          (receiverData?.lastName || "User")
-        }
-        username={receiverUsername}
-      />
-      <UserTag username={receiverUsername} />
-      <UserBio bio={receiverData.bio ?? ""} />
-      <Button
-        text="View Profile"
-        bg={true}
-        className="!w-fit mt-3 !px-8"
-        onClick={() => navigate(`/profile/${receiverData.username}`)}
-      />
-      <MdClose
-        className="absolute top-0 right-0 block lg:hidden text-2xl text-gray-500 cursor-pointer"
-        onClick={hideUserInfo}
-      />
-    </div>
-  );
-};
+    return (
+      <div className="relative flex flex-col items-center pt-8 lg:pt-0">
+        <UserImage
+          className="min-w-[9rem] w-36 min-h-[9rem] h-36 mb-4"
+          src={receiverData?.userPicture}
+          username={receiverUsername}
+          link={true}
+        />
+        <UserFullName
+          className="font-semibold text-xl"
+          fullName={
+            (receiverData?.firstName || "Socio") +
+            " " +
+            (receiverData?.lastName || "User")
+          }
+          username={receiverUsername}
+        />
+        <UserTag username={receiverUsername} />
+        <UserBio bio={receiverData?.bio ?? ""} />
+        <Button
+          text="View Profile"
+          bg={true}
+          className="!w-fit mt-3 !px-8"
+          onClick={() => navigate(`/profile/${receiverData?.username}`)}
+        />
+        <MdClose
+          className="absolute top-0 right-0 block lg:hidden text-2xl text-gray-500 cursor-pointer"
+          onClick={hideUserInfo}
+        />
+      </div>
+    );
+  }
+);
+
 export default ChatInfo;
