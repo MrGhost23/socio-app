265aa1950a22729f0e464dbe55cf6435653dfef9
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 09:28:41 +0300

    added tailwind-scrollbar classes to the ScrollableDiv component

--- frontend/src/ui/ScrollableDiv.tsx
+++ frontend/src/ui/ScrollableDiv.tsx
@@ -4,14 +4,11 @@ type Props = {
 };
 
 const ScrollableDiv: React.FC<Props> = (props) => {
+  const classes =
+    "pr-5 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-sky-500 hover:scrollbar-thumb-sky-600 scrollbar-track-gray-200 dark:scrollbar-track-primarylessDarker";
+
   return (
-    <div
-      className={
-        props.className
-          ? "pr-5 overflow-y-auto " + props.className
-          : "pr-5 overflow-y-auto"
-      }
-    >
+    <div className={props.className ? classes + " " + props.className : classes}>
       {props.children}
     </div>
   );
