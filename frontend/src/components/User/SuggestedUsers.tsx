202f5bc09e995261497576b4a9666ee54b3b7fd6
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-27 21:00:08 +0300

    fixed scroll in xl

--- frontend/src/components/User/SuggestedUsers.tsx
+++ frontend/src/components/User/SuggestedUsers.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const SuggestedUsers: React.FC<Props> = ({ users, socket }) => {
   return (
-    <div className="lg:max-h-[calc(70vh-80px)] pb-5 xl:pb-0 flex flex-row xl:flex-col gap-5 md:gap-8 xl:gap-3 overflow-x-auto lg:overflow-hidden scrollbar scrollbar-w-2 scrollbar-h-2 scrollbar-thumb-sky-500 hover:scrollbar-thumb-sky-600 scrollbar-track-gray-200 dark:scrollbar-track-primarylessDarker">
+    <div className="lg:max-h-[calc(70vh-80px)] pb-5 xl:pb-0 flex flex-row xl:flex-col gap-5 md:gap-8 xl:gap-3 overflow-x-auto lg:overflow-x-hidden scrollbar scrollbar-w-2 scrollbar-h-2 scrollbar-thumb-sky-500 hover:scrollbar-thumb-sky-600 scrollbar-track-gray-200 dark:scrollbar-track-primarylessDarker">
       {users.map((user) => (
         <User
           key={user.username}
