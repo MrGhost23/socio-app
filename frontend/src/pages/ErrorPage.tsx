9a717b6b791a203c4b796ab4ea42431da1888920
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 05:59:23 +0300

    created an error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,5 +1,18 @@
-const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+type Props = {
+  message?: string;
+};
+
+const ErrorPage: React.FC<Props> = ({ message }) => {
+  return (
+    <div className="mt-20 flex flex-col items-center justify-center gap-8">
+      <p className="text-4xl text-gray-600 dark:text-textLighter font-bold">
+        An error occurred!
+      </p>
+      <p className="text-2xl text-gray-500 dark:text-textLight font-semibold">
+        {message || "This page doesn't exist or you don't have access to it."}
+      </p>
+    </div>
+  );
 };
 
 export default ErrorPage;
