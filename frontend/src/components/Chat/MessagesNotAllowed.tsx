ed0aca8f18423a96d766b86ad799f3f3e8b7a024
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-27 16:11:07 +0300

    dark theme

--- frontend/src/components/Chat/MessagesNotAllowed.tsx
+++ frontend/src/components/Chat/MessagesNotAllowed.tsx
@@ -1,6 +1,6 @@
 const MessagesNotAllowed: React.FC = () => {
   return (
-    <div className="flex flex-row justify-center bg-gray-200 mt-5 py-4 font-semibold">
+    <div className="flex flex-row justify-center bg-gray-200 dark:bg-primarylessDark dark:text-textLighter mt-5 py-4 font-semibold">
       You can't contact this user
     </div>
   );
