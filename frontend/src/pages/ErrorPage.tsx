0af3d9ff9e7f4955e1818672009368a9cc6dff81
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 06:11:53 +0300

    removed the message prop from the error page

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,15 +1,11 @@
-type Props = {
-  message?: string;
-};
-
-const ErrorPage: React.FC<Props> = ({ message }) => {
+const ErrorPage: React.FC = () => {
   return (
     <div className="mt-20 flex flex-col items-center justify-center gap-8">
       <p className="text-4xl text-gray-600 dark:text-textLighter font-bold">
         An error occurred!
       </p>
       <p className="text-2xl text-gray-500 dark:text-textLight font-semibold">
-        {message || "This page doesn't exist or you don't have access to it."}
+        This page doesn't exist or you don't have access to it.
       </p>
     </div>
   );
