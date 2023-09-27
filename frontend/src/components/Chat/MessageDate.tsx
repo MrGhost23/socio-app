7a897370de9013e15e8d401139f1478ded7518bc
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2023-09-27 16:11:02 +0300

    dark theme

--- frontend/src/components/Chat/MessageDate.tsx
+++ frontend/src/components/Chat/MessageDate.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const MessageDate: React.FC<Props> = ({ date }) => {
   return (
-    <span className="w-fit text-xs sm:text-sm text-gray-600 tracking-tight">
+    <span className="w-fit text-xs sm:text-sm text-gray-600 tracking-tight dark:text-textLight">
       {formatDistanceToNow(new Date(date), {
         addSuffix: true,
       })}
