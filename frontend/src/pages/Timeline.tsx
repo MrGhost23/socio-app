b60f366946c562e9a570cdc03a036846fdd2b7b9
Heisenberg <omaradelawad20013@gmail.com>
2023-09-27 18:50:50 +0300

    used the hook in the Timeline page

--- frontend/src/pages/Timeline.tsx
+++ frontend/src/pages/Timeline.tsx
@@ -1,64 +1,41 @@
-import { useEffect, useState, useCallback } from "react";
 import { Socket } from "socket.io-client";
 import { PostType } from "../Types/Post.types";
 import PostForm from "../components/Post/PostForm";
 import Posts from "../components/Post/Posts";
 import PostsSkeleton from "../skeletons/PostsSkeleton";
-import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import NoDataMessage from "../components/NoDataMessage";
+import useInfiniteFetch from "../hooks/useInfiniteFetch";
 
 type Props = {
   socket: Socket;
 };
 
 const Timeline: React.FC<Props> = ({ socket }) => {
-  const [feedPosts, setFeedPosts] = useState<PostType[]>([]);
-  const [feedPostsIsLoading, setFeedPostsIsLoading] = useState(true);
-
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-
-  const fetchData = useCallback(
-    async (p?: number) => {
-      const response = await axios.get(
-        `http://localhost:5000/api/v1/posts?page=${p || page}`
-      );
-      const data = await response.data;
-
-      setFeedPosts((prevData) => [...prevData, ...data]);
-      setFeedPostsIsLoading(false);
-      setHasMore(data.length === 10);
-    },
-    [page]
+  const {
+    data: feedPosts,
+    loading: feedPostsIsLoading,
+    setData: setFeedPosts,
+    fetchMoreData: fetchMorePosts,
+    hasMore: feedPostsHasMore,
+    reFetch: reFetchPosts,
+  } = useInfiniteFetch<PostType>(
+    `http://localhost:5000/api/v1/posts`,
+    "get",
+    true
   );
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
-  const reFetchPosts = () => {
-    setFeedPostsIsLoading(true);
-    setFeedPosts([]);
-    setPage(1);
-    fetchData(1);
-  };
-
-  const fetchMorePosts = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
 
   const removePost = (postId: string) => {
     setFeedPosts((prevState) =>
-      prevState.filter((post: PostType) => post._id !== postId)
+      prevState!.filter((post: PostType) => post._id !== postId)
     );
   };
 
   const updatePost = (postId: string, description: string, image: string) => {
     setFeedPosts((prevState) => {
       const updatedPosts: PostType[] = [];
-      prevState.forEach((post) => {
+      prevState!.forEach((post) => {
         if (post._id === postId) {
           updatedPosts.push({
             ...post,
@@ -78,11 +55,11 @@ const Timeline: React.FC<Props> = ({ socket }) => {
       <PostForm fetchPosts={reFetchPosts} />
       {feedPostsIsLoading ? (
         <PostsSkeleton postsNumber={2} />
-      ) : feedPosts.length > 0 ? (
+      ) : feedPosts && feedPosts?.length > 0 ? (
         <InfiniteScroll
           dataLength={feedPosts.length}
           next={fetchMorePosts}
-          hasMore={hasMore}
+          hasMore={feedPostsHasMore}
           loader={<PostSkeleton className="mt-8" />}
         >
           <Posts
