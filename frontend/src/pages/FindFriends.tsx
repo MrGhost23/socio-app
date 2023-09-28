7a29a448a81354ab7b25eb8aaeea15553082cf5a
Heisenberg <omaradelawad20013@gmail.com>
2023-09-28 09:46:34 +0300

    added infinite scroll to the find friends page

--- frontend/src/pages/FindFriends.tsx
+++ frontend/src/pages/FindFriends.tsx
@@ -6,7 +6,9 @@ import User from "../components/User/User";
 import UsersSkeleton from "../skeletons/UsersSkeleton";
 import { Socket } from "socket.io-client";
 import NoDataMessage from "../components/NoDataMessage";
-import useInfiniteFetch from '../hooks/useInfiniteFetch';
+import useInfiniteFetch from "../hooks/useInfiniteFetch";
+import InfiniteScroll from "react-infinite-scroll-component";
+import PostSkeleton from "../skeletons/PostSkeleton";
 
 type Props = {
   socket: Socket;
@@ -18,6 +20,8 @@ const FindFriends: React.FC<Props> = ({ socket }) => {
   const {
     data: suggestedUsers,
     loading: suggestedUsersIsLoading,
+    hasMore: followingHasMore,
+    fetchMoreData: fetchMoreFollowing,
   } = useInfiniteFetch<UserType>(
     `http://localhost:5000/api/v1/users/${currentUser!.username}/find-friends`,
     "get",
@@ -37,21 +41,28 @@ const FindFriends: React.FC<Props> = ({ socket }) => {
       ) : (
         <Card className="p-8 !text-left">
           <h3 className="mb-5 text-xl">Find Friends</h3>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4">
-            {suggestedUsers?.length ? (
-              suggestedUsers.map((user) => (
-                <User
-                  key={user.username}
-                  user={user}
-                  changeStyle={false}
-                  mode="follow"
-                  socket={socket}
-                />
-              ))
-            ) : (
-              <NoDataMessage message="Found no users to suggest" />
-            )}
-          </div>
+          {suggestedUsers && suggestedUsers?.length > 0 ? (
+            <InfiniteScroll
+              dataLength={suggestedUsers.length}
+              next={fetchMoreFollowing}
+              hasMore={followingHasMore}
+              loader={<PostSkeleton className="mt-8" />}
+            >
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4">
+                {suggestedUsers.map((user) => (
+                  <User
+                    key={user.username}
+                    user={user}
+                    changeStyle={false}
+                    mode="follow"
+                    socket={socket}
+                  />
+                ))}
+              </div>
+            </InfiniteScroll>
+          ) : (
+            <NoDataMessage message="Found no users to suggest" />
+          )}
         </Card>
       )}
     </>
