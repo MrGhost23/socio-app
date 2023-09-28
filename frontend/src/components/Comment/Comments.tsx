e1f719762f36b049f98805d01c8a2910a4cafcf1
Heisenberg <omaradelawad20013@gmail.com>
2023-09-28 08:14:01 +0300

    refactored the comments component and added show less button

--- frontend/src/components/Comment/Comments.tsx
+++ frontend/src/components/Comment/Comments.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaChevronUp } from "react-icons/fa";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { Comment as CommentType } from "../../Types/Comment.types";
 import Comment from "./Comment";
 
@@ -7,34 +7,65 @@ type Props = {
   comments: CommentType[];
   removeCommentFunction: (commentId: string) => void;
   editCommentFunction: (commentId: string, text: string) => void;
+  fetchMoreComments: () => void;
+  commentsHasMore: boolean;
 };
 
 const Comments: React.FC<Props> = ({
   comments,
   removeCommentFunction,
   editCommentFunction,
+  fetchMoreComments,
+  commentsHasMore,
 }) => {
   const max = 2;
   const [commentsSliced, setCommentsSliced] = useState(comments?.length > max);
 
-  const showAll = () => {
-    setCommentsSliced(false);
+  const showMore = () => {
+    if (commentsSliced) {
+      setCommentsSliced(false);
+    } else if (commentsHasMore) {
+      fetchMoreComments();
+    }
+  };
+
+  const showLess = () => {
+    setCommentsSliced(true);
   };
 
   if (!comments) return;
 
   return (
     <div className="mb-5 flex flex-col gap-4">
-      {commentsSliced ? (
-        <>
-          <p
-            className="flex flex-row items-center gap-2 text-base text-gray-500 font-semibold cursor-pointer transition duration-500 hover:text-sky-500"
-            onClick={showAll}
-          >
-            <FaChevronUp />
-            Show all
-          </p>
-          {comments.slice(-2).map((comment) => (
+      {commentsSliced || commentsHasMore ? (
+        <p
+          className="flex flex-row items-center gap-2 text-base text-gray-500 font-semibold cursor-pointer transition duration-500 hover:text-sky-500 dark:text-gray-300 dark:hover:text-sky-500"
+          onClick={showMore}
+        >
+          <FaChevronUp />
+          Show More
+        </p>
+      ) : (
+        <p
+          className="flex flex-row items-center gap-2 text-base text-gray-500 font-semibold cursor-pointer transition duration-500 hover:text-sky-500 dark:text-gray-300 dark:hover:text-sky-500"
+          onClick={showLess}
+        >
+          <FaChevronDown />
+          Show Less
+        </p>
+      )}
+      {commentsSliced
+        ? comments
+            .slice(-2)
+            .map((comment) => (
+              <Comment
+                key={comment._id}
+                comment={comment}
+                removeCommentFunction={removeCommentFunction}
+                editCommentFunction={editCommentFunction}
+              />
+            ))
+        : comments?.map((comment) => (
             <Comment
               key={comment._id}
               comment={comment}
@@ -42,17 +73,6 @@ const Comments: React.FC<Props> = ({
               editCommentFunction={editCommentFunction}
             />
           ))}
-        </>
-      ) : (
-        comments?.map((comment) => (
-          <Comment
-            key={comment._id}
-            comment={comment}
-            removeCommentFunction={removeCommentFunction}
-            editCommentFunction={editCommentFunction}
-          />
-        ))
-      )}
     </div>
   );
 };
