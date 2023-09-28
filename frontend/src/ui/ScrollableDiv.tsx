c0fafc37055fc2ec5277a4444ab2a86069a12e4f
Heisenberg <omaradelawad20013@gmail.com>
2023-09-28 10:34:03 +0300

    added an id optional prop to the ScrollableDiv ui component

--- frontend/src/ui/ScrollableDiv.tsx
+++ frontend/src/ui/ScrollableDiv.tsx
@@ -1,4 +1,5 @@
 type Props = {
+  id?: string;
   className?: string;
   children: React.ReactNode;
 };
@@ -8,7 +9,10 @@ const ScrollableDiv: React.FC<Props> = (props) => {
     "pr-5 overflow-y-auto scrollbar scrollbar-w-2 scrollbar-thumb-sky-500 hover:scrollbar-thumb-sky-600 scrollbar-track-gray-200 dark:scrollbar-track-primarylessDarker";
 
   return (
-    <div className={props.className ? classes + " " + props.className : classes}>
+    <div
+      id={props.id}
+      className={props.className ? classes + " " + props.className : classes}
+    >
       {props.children}
     </div>
   );
